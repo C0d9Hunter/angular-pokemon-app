79fb11108baf2f89f645996faa25311e6a36a468
C0d9Hunter <jocelynmarceltoe@gmail.com>
2023-07-01 18:09:49 +0000

    pokemon apps angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {LoginComponent} from "./login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"login" , pathMatch:"full"},
+  {path:"login", component:LoginComponent},
+  {path:"**", component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
